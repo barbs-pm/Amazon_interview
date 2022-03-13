18ebe5c0c7ec13a9de574f5c477b756c67215988
barbs-pm <barbarapmarkus@gmail.com>
2022-03-12 23:10:53 -0300

    add ex 7 bsf and dfs

---
+++ patterns/7-BFS&DFS.py
@@ -0,0 +1,35 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+visited = []   # List to keep track of visited nodes.
+queue = []     # Initialize a queue
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+    
+    while queue:
+        s = queue.pop(0) 
+        print (s, end = " ") 
+        
+        for neighbor in graph[s]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.append(node)
+        for neighbor in graph[node]:
+            dfs(visited, graph, neighbor)
+
+# Driver Code
+bfs(visited, graph, 'B')
+dfs(visited, graph, 'B')
